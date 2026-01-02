f69c2706bd70424361cee6eff97992747dcafc10
sanganihet649-cloud <sanganihet649@gmail.com>
2026-01-02 10:00:19 +0530

    Add About Us page with hotel details

---
+++ about as.php
@@ -0,0 +1,132 @@
+<?php
+include('header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patel Hotel - About Us</title>
+    <?php require('links.php') ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <style>
+        .box{
+            border-top-color: var(--teal) !important;
+        }
+    </style>
+
+</head>
+
+<body class="bg-light">
+
+<div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">ABOUT Us</h2>
+        <hr>
+        <!-- <div class="h-line text-center"> -->
+        <p align="center">"Enjoy Your journey."</p>
+        <!-- </div> -->
+    </div>
+
+  
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-5 mb-4 justify-content-between align-items-center order-lg-1  order-md-1 order-2" >
+                <h3 class="mb-3">The Unparalleled
+                Guardian of Grandeur</h3>
+                <p>
+                Built on a vision of grandeur, Patel Hotel conjures a panoply of superlative experiences that are envisioned to indulge and forge unforgettable memories.
+                </p>
+
+            </div>
+
+            <div class="col-lg-4 col-md-5  mb-4 align-items-right order-lg-2 order-md-2 order-1">
+                <img src="images/hotel2.jpg" class="w-100">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white-rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/hotel.png" alt="" height="70px">
+                    <h4 class="mt-3">100+ ROOMS</h4>
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white-rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/customer.jpg" alt="" height="70px">
+                    <h4 class="mt-3">200+ CUSTOMERS</h4>
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white-rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/review.jpg" alt="" height="70px">
+                    <h4 class="mt-3">350+ REVIEW</h4>
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4 px-4">
+                <div class="bg-white-rounded shadow p-4 border-top border-4 text-center box">
+                    <img src="images/staff.jpg" alt="" height="70px">
+                    <h4 class="mt-3">200+ STAFF</h4>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    
+
+    <!--footer-->
+
+    <?php require('footer.php') ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script>
+        var swiper = new Swiper(".swiper-container", {
+            spaceBetween: 30,
+            effect: "fade",
+            loop: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            }
+        });
+        var swiper = new Swiper(".swiper-testimonials", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            slidesPerView: "3",
+            loop: true,
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        });
+    </script>
+
+</body>
+
+</html>
