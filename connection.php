892efef9478b55f1451ac963442df53ace64d1eb
sanganihet649-cloud <sanganihet649@gmail.com>
2026-01-02 10:03:25 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,14 @@
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "Shreehotel";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+
+
+
+
+    ?>
