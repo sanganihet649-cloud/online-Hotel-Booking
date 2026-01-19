6c451ead3315d18bfd258c9f4cbb0d6e5d7bd6ae
sanganihet649-cloud <sanganihet649@gmail.com>
2026-01-19 10:28:09 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,1 @@
-    <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "Shreehotel";
-
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-?>
 
